db250c40c2d4b934441b2caa1094939490319be2
FtRookie <email@ftrookie.com>
2026-05-03 03:24:50 -0700

    add mods remove roblox engineer thing

--- src/engine/shared/PlayerRank.ts
+++ src/engine/shared/PlayerRank.ts
@@ -1,18 +1,6 @@
 import { RunService } from "@rbxts/services";
-import { Throttler } from "engine/shared/Throttler";
-
 export namespace PlayerRank {
-	export const developers: number[] = [];
-
-	export function isRobloxEngineer(player: Player): boolean {
-		const req = Throttler.retryOnFail<boolean>(3, 1, () => player.IsInGroup(1200769));
-
-		if (!req.success) {
-			warn(req.error_message);
-		}
-
-		return req.success ? req.message : false;
-	}
+	export const developers: number[] = [10897692300, 238427763];
 
 	export function isAdmin(player: Player): boolean {
 		if (RunService.IsStudio()) return true;

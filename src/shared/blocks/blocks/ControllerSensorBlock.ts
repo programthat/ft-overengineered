a01f0e91d3fe5e6e998860d78f4242916ffb8101
samlovebutter <dima_eremen@mail.ru>
2026-06-09 09:57:40 +0300

    unyandere-dev the new controller block

--- src/shared/blocks/blocks/ControllerSensorBlock.ts
+++ src/shared/blocks/blocks/ControllerSensorBlock.ts
@@ -76,6 +76,22 @@ const definition = {
 	},
 } satisfies BlockLogicFullBothDefinitions;
 
+// Gamepad mappings
+const buttonOutputs = [
+	[Enum.KeyCode.DPadUp, "DPadUp"],
+	[Enum.KeyCode.DPadDown, "DPadDown"],
+	[Enum.KeyCode.DPadLeft, "DPadLeft"],
+	[Enum.KeyCode.DPadRight, "DPadright"],
+	[Enum.KeyCode.ButtonL1, "LeftBumper"],
+	[Enum.KeyCode.ButtonR1, "RightBumper"],
+	[Enum.KeyCode.ButtonL3, "LeftStickButton"],
+	[Enum.KeyCode.ButtonR3, "RightStickButton"],
+	[Enum.KeyCode.ButtonA, "A"],
+	[Enum.KeyCode.ButtonB, "B"],
+	[Enum.KeyCode.ButtonX, "X"],
+	[Enum.KeyCode.ButtonY, "Y"],
+] as const;
+
 export type { Logic as ControllerSensorBlockLogic };
 
 @injectable
@@ -85,142 +101,63 @@ class Logic extends BlockLogic<typeof definition> {
 	constructor(block: BlockLogicArgs) {
 		super(definition, block);
 
-		let leftStick = Vector3.zero;
-		let rightStick = Vector3.zero;
-
-		let leftTrigger = 0;
-		let rightTrigger = 0;
-
-		let leftStickButton = false;
-		let rightStickButton = false;
-
-		let dPadUp = false;
-		let dPadDown = false;
-		let dPadLeft = false;
-		let dPadRight = false;
-
-		let leftBumper = false;
-		let rightBumper = false;
-
-		let buttonA = false;
-		let buttonB = false;
-		let buttonX = false;
-		let buttonY = false;
-
 		this.onk(["squareMode"], ({ squareMode }) => {
 			this.squareMode = squareMode;
 		});
 
-		if (RunService.IsClient()) {
-			UserInputService.InputChanged.Connect((input, gameProcessed) => {
-				if (gameProcessed) return;
-				if (input.UserInputType !== Enum.UserInputType.Gamepad1) return;
+		this.onTicc(() => {
+			if (!RunService.IsClient()) return;
+			const gamepadState = UserInputService.GetGamepadState(Enum.UserInputType.Gamepad1);
 
-				if (input.KeyCode === Enum.KeyCode.Thumbstick1) {
+			let leftStick = Vector3.zero;
+			let rightStick = Vector3.zero;
+			let leftTrigger = 0;
+			let rightTrigger = 0;
+			const pressed = new Set<Enum.KeyCode>();
+
+			for (const input of gamepadState) {
+				const key = input.KeyCode;
+				if (key === Enum.KeyCode.Thumbstick1) {
 					let v = new Vector2(input.Position.X, input.Position.Y);
 					if (this.squareMode) v = applySquareMapping(v.X, v.Y);
 					leftStick = new Vector3(v.X, v.Y, 0);
-				} else if (input.KeyCode === Enum.KeyCode.Thumbstick2) {
+					continue;
+				}
+
+				if (key === Enum.KeyCode.Thumbstick2) {
 					let v = new Vector2(input.Position.X, input.Position.Y);
 					if (this.squareMode) v = applySquareMapping(v.X, v.Y);
 					rightStick = new Vector3(v.X, v.Y, 0);
+					continue;
 				}
 
-				if (input.KeyCode === Enum.KeyCode.ButtonL2) {
+				if (key === Enum.KeyCode.ButtonL2) {
 					leftTrigger = input.Position.Z;
+					continue;
 				}
 
-				if (input.KeyCode === Enum.KeyCode.ButtonR2) {
+				if (key === Enum.KeyCode.ButtonR2) {
 					rightTrigger = input.Position.Z;
+					continue;
 				}
-			});
-		}
 
-		this.onTicc(() => {
-			if (RunService.IsClient()) {
-				const gamepadState = UserInputService.GetGamepadState(Enum.UserInputType.Gamepad1);
-
-				leftStickButton = false;
-				rightStickButton = false;
-
-				dPadUp = false;
-				dPadDown = false;
-				dPadLeft = false;
-				dPadRight = false;
-
-				leftBumper = false;
-				rightBumper = false;
-
-				buttonA = false;
-				buttonB = false;
-				buttonX = false;
-				buttonY = false;
-
-				for (const input of gamepadState) {
-					if (input.KeyCode === Enum.KeyCode.Thumbstick1) {
-						let v = new Vector2(input.Position.X, input.Position.Y);
-						if (this.squareMode) v = applySquareMapping(v.X, v.Y);
-						leftStick = new Vector3(v.X, v.Y, 0);
-					} else if (input.KeyCode === Enum.KeyCode.Thumbstick2) {
-						let v = new Vector2(input.Position.X, input.Position.Y);
-						if (this.squareMode) v = applySquareMapping(v.X, v.Y);
-						rightStick = new Vector3(v.X, v.Y, 0);
-					} else if (input.KeyCode === Enum.KeyCode.ButtonL2) {
-						leftTrigger = input.Position.Z;
-					} else if (input.KeyCode === Enum.KeyCode.ButtonR2) {
-						rightTrigger = input.Position.Z;
-					} else if (input.KeyCode === Enum.KeyCode.ButtonL3) {
-						if (
-							input.UserInputState === Enum.UserInputState.Begin ||
-							input.UserInputState === Enum.UserInputState.Change
-						) {
-							leftStickButton = true;
-						}
-					} else if (input.KeyCode === Enum.KeyCode.ButtonR3) {
-						if (
-							input.UserInputState === Enum.UserInputState.Begin ||
-							input.UserInputState === Enum.UserInputState.Change
-						) {
-							rightStickButton = true;
-						}
-					} else if (
-						input.UserInputState === Enum.UserInputState.Begin ||
-						input.UserInputState === Enum.UserInputState.Change
-					) {
-						if (input.KeyCode === Enum.KeyCode.DPadUp) dPadUp = true;
-						else if (input.KeyCode === Enum.KeyCode.DPadDown) dPadDown = true;
-						else if (input.KeyCode === Enum.KeyCode.DPadLeft) dPadLeft = true;
-						else if (input.KeyCode === Enum.KeyCode.DPadRight) dPadRight = true;
-						else if (input.KeyCode === Enum.KeyCode.ButtonL1) leftBumper = true;
-						else if (input.KeyCode === Enum.KeyCode.ButtonR1) rightBumper = true;
-						else if (input.KeyCode === Enum.KeyCode.ButtonA) buttonA = true;
-						else if (input.KeyCode === Enum.KeyCode.ButtonB) buttonB = true;
-						else if (input.KeyCode === Enum.KeyCode.ButtonX) buttonX = true;
-						else if (input.KeyCode === Enum.KeyCode.ButtonY) buttonY = true;
-					}
+				if (
+					input.UserInputState === Enum.UserInputState.Begin ||
+					input.UserInputState === Enum.UserInputState.Change
+				) {
+					pressed.add(key);
+					continue;
 				}
 			}
 
-			this.output["LeftStick"].set("vector3", leftStick);
-			this.output["LeftStickButton"].set("bool", leftStickButton);
-			this.output["LeftTrigger"].set("number", leftTrigger);
+			this.output.LeftStick.set("vector3", leftStick);
+			this.output.RightStick.set("vector3", rightStick);
+			this.output.LeftTrigger.set("number", leftTrigger);
+			this.output.RightTrigger.set("number", rightTrigger);
 
-			this.output["RightStick"].set("vector3", rightStick);
-			this.output["RightStickButton"].set("bool", rightStickButton);
-			this.output["RightTrigger"].set("number", rightTrigger);
-
-			this.output["DPadUp"].set("bool", dPadUp);
-			this.output["DPadDown"].set("bool", dPadDown);
-			this.output["DPadLeft"].set("bool", dPadLeft);
-			this.output["DPadright"].set("bool", dPadRight);
-
-			this.output["LeftBumper"].set("bool", leftBumper);
-			this.output["RightBumper"].set("bool", rightBumper);
-
-			this.output["A"].set("bool", buttonA);
-			this.output["B"].set("bool", buttonB);
-			this.output["X"].set("bool", buttonX);
-			this.output["Y"].set("bool", buttonY);
+			for (const [key, out] of buttonOutputs) {
+				this.output[out].set("bool", pressed.has(key));
+			}
 		});
 	}
 }

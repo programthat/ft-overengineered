1098cbbba0b05eeed8f2459f620ff7372b69289a
FtRookie <email@ftrookie.com>
2026-06-11 03:20:35 -0700

    Gravity sensor use player config

--- src/shared/blocks/blocks/GravitySensorBlock.ts
+++ src/shared/blocks/blocks/GravitySensorBlock.ts
@@ -2,6 +2,7 @@ import { RunService } from "@rbxts/services";
 import { InstanceBlockLogic } from "shared/blockLogic/BlockLogic";
 import { BlockCreation } from "shared/blocks/BlockCreation";
 import { Physics } from "shared/Physics";
+import type { PlayerDataStorage } from "client/PlayerDataStorage";
 import type { BlockLogicFullBothDefinitions, InstanceBlockLogicArgs } from "shared/blockLogic/BlockLogic";
 import type { BlockBuilder } from "shared/blocks/Block";
 
@@ -16,14 +17,18 @@ const definition = {
 } satisfies BlockLogicFullBothDefinitions;
 
 export type { Logic as GravitySensorBlockLogic };
+@injectable
 class Logic extends InstanceBlockLogic<typeof definition> {
-	constructor(block: InstanceBlockLogicArgs) {
+	constructor(block: InstanceBlockLogicArgs, @tryInject playerConfig?: PlayerDataStorage) {
 		super(definition, block);
 
 		this.event.subscribe(RunService.PostSimulation, () => {
 			this.output.result.set(
 				"number",
-				Physics.GetGravityOnHeight(Physics.LocalHeight.fromGlobal(this.instance.GetPivot().Y)),
+				Physics.GetGravityOnHeight(
+					Physics.LocalHeight.fromGlobal(this.instance.GetPivot().Y),
+					playerConfig?.config.get().physics.customGravity,
+				),
 			);
 		});
 	}
